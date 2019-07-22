4654b25febb46943f1a7eb3ec39998a6204e7708
Filipe Corrêa <filipecorrea@br.ibm.com>
2019-07-22 10:12:43 -0300

    feat(imagecard): add expand button

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -18,7 +18,7 @@ const ImageCard = ({ title, content, content: { data: image }, size, ...others }
   const availableActions = { expand: supportedSize };
 
   return (
-    <Card title={title} size={size} availableActions={availableActions} {...others}>
+    <Card title={title} size={size} availableActions={availableActions} isExpanded {...others}>
       {!others.isLoading ? (
         <ContentWrapper>
           {supportedSize ? (
